e9826448c7c731ae77632686442a41a654c6d441
Chidinma Okafor <fildaokafor@gmail.com>
2017-02-23 17:49:50 +0000

    none

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quiz System</title>
+</head>
+<body>
+<?php include page1.html ?>
+<header>
+    <h1>FILDA OKAFOR QUIZ SYSTEM</h1>
+    <main>
+    <p>Welcome to my quiz system. To start quiz click the button below</p>
+        <p><a href ="page1.html">Start Quiz</a></p>
+</main>
+    <footer>
+        <p> (c) filda concept</p>
+    </footer>
+</header>
+</body>
+</html>
